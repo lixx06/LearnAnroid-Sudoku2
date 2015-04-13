64991fb5b334a97ab77e0d8d859d7a556f911827
lixx <lixx06@gmail.com>
2015-04-13 23:58:13 +0800

    学习androi首个例程,此工程目前为只可显示About对话框的界面

---
+++ src/org/example/sudoku2/MainActivity.java
@@ -0,0 +1,53 @@
+package org.example.sudoku2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+     // Set up click listeners for all the buttons
+        /*View continueButton = this.findViewById(R.id.continue_button);
+        continueButton.setOnClickListener( this);
+        View newButton = this.findViewById(R.id.new_button);
+        newButton.setOnClickListener( this);*/
+        View aboutButton = this.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        /*View exitButton = this.findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);  */      
+        
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+
+	public void onClick(View v) {		
+		switch (v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		}
+	}
+	
+	
+}
+
+
+
+
