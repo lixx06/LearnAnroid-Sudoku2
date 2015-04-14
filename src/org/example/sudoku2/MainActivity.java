fa17ae19867260eb2a2ae5fa906b32187dcdbfed
lixx <lixx06@gmail.com>
2015-04-14 22:13:17 +0800

    create the menu setting

--- src/org/example/sudoku2/MainActivity.java
+++ src/org/example/sudoku2/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -36,6 +37,17 @@ public class MainActivity extends Activity implements OnClickListener {
         return true;
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.settings:
+    		startActivity(new Intent(this, Settings.class));
+    		return true;
+    		// More items go here (if any) ...
+    	}
+    	return false;
+    }
+
 
 	public void onClick(View v) {		
 		switch (v.getId()) {
