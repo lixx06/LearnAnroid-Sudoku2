61a4c3ab39b59f03ee8383f152e1b09676e26912
lixx <lixx06@gmail.com>
2015-04-14 19:01:23 +0800

    fix the bug when rotate the phone

--- gen/org/example/sudoku2/R.java
+++ gen/org/example/sudoku2/R.java
@@ -29,10 +29,9 @@ public final class R {
     public static final class id {
         public static final int about_button=0x7f090003;
         public static final int about_content=0x7f090000;
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090005;
         public static final int continue_button=0x7f090001;
         public static final int exit_button=0x7f090004;
-        public static final int new_button=0x7f090005;
         public static final int new_game_button=0x7f090002;
     }
     public static final class layout {
